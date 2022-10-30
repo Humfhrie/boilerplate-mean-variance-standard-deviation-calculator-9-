7969ffc3b6bf7f6384eacd82c6c3e214db0784c2
Humfhrie <114100121+Humfhrie@users.noreply.github.com>
2022-10-30 10:58:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import mean_var_std
-from unittest import main
+import numpy as np
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+def calculate(input):
+  if len(input) != 9:
+    raise ErrorValue ("list must contain nine numbers.")
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+    x = np.array(input).reshape(3,3)
+    print(x)
+    print(x.mean(0), x.mean(1), x.mean())
+
+    cal = dict()
+    cal['mean'] = [x.mean(0).tolist(), x.mean(1).tolist(), x.mean()]
+    cal['variance'] = [x.var(0).tolist(), x.var(1).tolist(), x.var()]
+    cal['stadard deviation'] = [x.std(0).tolist(), x.std(1).tolist(), x.std()]
+    cal['max'] = [x.max(0).tolist(), x.max(1).tolist(), x.max()]
+    cal['min'] = [x.min(0).tolist(), x.min(1).tolist(), x.min()]
+    cal['sum'] = [x.sum(0).tolist(), x.sum(1).tolist(), x.sum()]
+    return cal
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
